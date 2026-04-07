00d0994f96bb8f57fa590caa688cfdd83c47774f
nattaly-v <nvargass@ucdavis.edu>
2026-04-07 10:52:08 -0700

    desktop portal page

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
-(function() {
+(function () {
     'use strict';
 
     const button = document.querySelector('button');
     const body = document.querySelector('body');
     const banner = document.querySelector('#banner');
-    const sections = document.querySelectorAll('section')
+    const bannerImg = document.querySelector('#banner-img');
+    const sections = document.querySelectorAll('section');
     let mode = 'dark';
 
-    button.addEventListener('click', function() {
+    button.addEventListener('click', function () {
         if (mode === 'dark') {
             body.className = 'switch';
             banner.className = 'switch';
@@ -15,6 +16,8 @@
             for (const section of sections) {
                 section.className = 'switch';
             }
+            bannerImg.src = 'images/night-view.png'; // swap to dark image
+            bannerImg.alt = 'night view';
             mode = 'light';
         } else {
             body.removeAttribute('class');
@@ -23,7 +26,9 @@
             for (const section of sections) {
                 section.removeAttribute('class');
             }
-            mode = 'dark'
+            bannerImg.src = 'images/day-view.jpg'; //swap back
+            bannerImg.alt = 'day view';
+            mode = 'dark';
         }
-    })
-})()
+    });
+})();
